81cb6ebd514695594a9314e82f65e57f2fb99b63
Heim_Sendir <alex.noa29@gamil.com>
2024-10-03 12:42:54 +0500

    First commit

---
+++ bot.py
@@ -0,0 +1,16 @@
+import telebot
+
+# Вставь сюда свой токен
+API_TOKEN = '727421163:AAGdQxls1CdRAJIrDpXa5HwAmfsMd5w5-7k'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# Простая функция для ответа на любое сообщение
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+# Запуск бота
+bot.polling()
